c85cac2738b6dfdbaa8ca6e94b7c175c5637eadb
janyousssef <89477@must.edu.eg.com>
2023-02-19 06:24:50 +0200

    properties now appear in correct order

--- src/main/java/com/example/restcinema/models/Cinema.java
+++ src/main/java/com/example/restcinema/models/Cinema.java
@@ -1,15 +1,21 @@
 package com.example.restcinema.models;
 
-public class Cinema {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonPropertyOrder(value = {"total_rows", "total_columns", "available_seats"})
+public class Cinema {
+    @JsonProperty("total_rows")
     private final Integer TOTAL_ROWS = 9;
+    @JsonProperty("total_columns")
     private final Integer TOTAL_COLUMNS = 9;
+    @JsonProperty("available_seats")
     Seat[] availableSeats = new Seat[TOTAL_ROWS * TOTAL_COLUMNS];
 
     public Cinema() {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                availableSeats[i * TOTAL_ROWS + j ] = new Seat(i + 1, j + 1);
+                availableSeats[i * TOTAL_ROWS + j] = new Seat(i + 1, j + 1);
             }
         }
     }
@@ -21,6 +27,7 @@ public class Cinema {
     public Integer getTOTAL_COLUMNS() {
         return TOTAL_COLUMNS;
     }
+
     public Seat[] getAvailableSeats() {
         return availableSeats;
     }
