3ac0617384db35dd6d9e898b9bdfcabe76567d6f
janyousssef <89477@must.edu.eg.com>
2023-02-19 05:35:34 +0200

    clean start

---
+++ src/main/java/com/example/restcinema/ResTcinemaApplication.java
@@ -0,0 +1,13 @@
+package com.example.restcinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTcinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTcinemaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restcinema/ResTcinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restcinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTcinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
