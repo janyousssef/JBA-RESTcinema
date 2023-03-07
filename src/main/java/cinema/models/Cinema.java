b7fe7f1fb1d3c2bce0ea02865b6e36b166d9313a
janyousssef <89477@must.edu.eg.com>
2023-03-07 17:07:50 +0200

    use factory methods instead of directly calling constructor

--- src/main/java/cinema/models/Cinema.java
+++ src/main/java/cinema/models/Cinema.java
@@ -1,6 +1,5 @@
 package cinema.models;
 
-import cinema.services.CinemaService;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
@@ -12,9 +11,6 @@ public class Cinema {
     private final Integer TOTAL_COLUMNS;
     @JsonProperty("available_seats")
     private final Seat[] seats;
-    private final CinemaService cinemaService = new CinemaService(this);
-    long HIGH_PRICE = 10L;
-    long LOW_PRICE = 8L;
 
     public Cinema(Integer TOTAL_ROWS, Integer TOTAL_COLUMNS) {
         this.TOTAL_ROWS = TOTAL_ROWS;
@@ -29,9 +25,9 @@ public class Cinema {
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
                 if (row < 4) {
-                    seat = new Seat(row + 1, col + 1, HIGH_PRICE, false);
+                    seat = Seat.withHighPrice(row + 1, col + 1);
                 } else {
-                    seat = new Seat(row + 1, col + 1, LOW_PRICE, false);
+                    seat = Seat.withLowPrice(row + 1, col + 1);
                 }
 
                 seats[row * TOTAL_ROWS + col] = seat;
