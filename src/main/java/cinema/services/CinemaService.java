37e75cadc9e2bd21398c8bb0873d4310bea7af29
janyousssef <89477@must.edu.eg.com>
2023-03-07 16:54:54 +0200

    moved logic from cinema class to new cinema service class

---
+++ src/main/java/cinema/services/CinemaService.java
@@ -0,0 +1,30 @@
+package cinema.services;
+
+import cinema.models.Cinema;
+import cinema.models.Seat;
+
+public class CinemaService {
+    private final Cinema cinema;
+
+    public CinemaService(Cinema cinema) {
+        this.cinema = cinema;
+    }
+
+    public Cinema getCinema() {
+        return cinema;
+    }
+
+    public Seat getSeat(Integer row, Integer col) {
+        row--;
+        col--;
+        return cinema.getSeats()[row * cinema.getTOTAL_ROWS() + col];
+    }
+
+    public Seat reserveSeat(Seat seat) {
+        return seat.reserve();
+    }
+
+    public Integer getSeatIndex(Seat seat) {
+        return seat.getRow() * cinema.getTOTAL_ROWS() + seat.getColumn();
+    }
+}
