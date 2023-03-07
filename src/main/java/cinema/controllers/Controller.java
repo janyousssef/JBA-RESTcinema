6823919176311006e10deaac003e4c988e4a0430
janyousssef <89477@must.edu.eg.com>
2023-03-07 16:55:27 +0200

    refactored existing classes to support last change

--- src/main/java/cinema/controllers/Controller.java
+++ src/main/java/cinema/controllers/Controller.java
@@ -3,6 +3,7 @@ package cinema.controllers;
 import cinema.errors.SeatPurchaseProblem;
 import cinema.models.Cinema;
 import cinema.models.Seat;
+import cinema.services.CinemaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,16 +12,16 @@ import org.springframework.web.bind.annotation.*;
 public class Controller {
     final String INVALID_SEAT = "The number of a row or a column is out of bounds!";
     final String ALREADY_PURCHASED = "The ticket has been already purchased!";
-    Cinema cinema = new Cinema(9, 9);
+    CinemaService cinemaService = new CinemaService(new Cinema(9, 9));
 
     @GetMapping("/seats")
     public ResponseEntity<Cinema> getAllSeats() {
-        return new ResponseEntity<>(cinema, HttpStatus.OK);
+        return new ResponseEntity<>(cinemaService.getCinema(), HttpStatus.OK);
     }
 
     @GetMapping("/seat")
     public ResponseEntity<Seat> getSeatByRowCol(@RequestParam Integer row, @RequestParam Integer col) {
-        Seat seat = cinema.getSeat(row, col);
+        Seat seat = cinemaService.getSeat(row, col);
         return new ResponseEntity<>(seat, HttpStatus.OK);
     }
 
@@ -30,11 +31,11 @@ public class Controller {
         if (seat.invalidInstance())
             return new ResponseEntity<>(new SeatPurchaseProblem(INVALID_SEAT), HttpStatus.BAD_REQUEST);
 
-        seat = cinema.getSeat(seat.getRow(), seat.getColumn());
+        seat = cinemaService.getSeat(seat.getRow(), seat.getColumn());
         if (seat.isReserved())
             return new ResponseEntity<>(new SeatPurchaseProblem(ALREADY_PURCHASED), HttpStatus.BAD_REQUEST);
 
-        return new ResponseEntity<>(cinema.reserveSeat(seat), HttpStatus.OK);
+        return new ResponseEntity<>(cinemaService.reserveSeat(seat), HttpStatus.OK);
 
     }
 
--- src/main/java/cinema/models/Cinema.java
+++ src/main/java/cinema/models/Cinema.java
@@ -1,5 +1,6 @@
 package cinema.models;
 
+import cinema.services.CinemaService;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
@@ -11,7 +12,9 @@ public class Cinema {
     private final Integer TOTAL_COLUMNS;
     @JsonProperty("available_seats")
     private final Seat[] seats;
-
+    private final CinemaService cinemaService = new CinemaService(this);
+    long HIGH_PRICE = 10L;
+    long LOW_PRICE = 8L;
 
     public Cinema(Integer TOTAL_ROWS, Integer TOTAL_COLUMNS) {
         this.TOTAL_ROWS = TOTAL_ROWS;
@@ -25,21 +28,24 @@ public class Cinema {
 
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
-                if (row < 4) seat = new Seat(row + 1, col + 1, 10L, false);
-                else seat = new Seat(row + 1, col + 1, 8L, false);
+                if (row < 4) {
+                    seat = new Seat(row + 1, col + 1, HIGH_PRICE, false);
+                } else {
+                    seat = new Seat(row + 1, col + 1, LOW_PRICE, false);
+                }
 
                 seats[row * TOTAL_ROWS + col] = seat;
             }
         }
     }
 
-    public Seat getSeat(Integer row, Integer col) {
-        row--;
-        col--;
-        return seats[row * TOTAL_ROWS + col];
+    public Integer getTOTAL_ROWS() {
+        return TOTAL_ROWS;
     }
 
-    public Seat reserveSeat(Seat seat) {
-        return seat.reserve();
+    public Seat[] getSeats() {
+        return seats;
     }
+
+
 }
